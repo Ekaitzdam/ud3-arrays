495b0d38e3935a1b2fc73e7939804269d56c03bc
Unknown <ekaitznavarro@gmail.com>
2017-11-29 21:38:03 +0100

    Ejercicio 4 mejorado

--- 04_matriz_3x3/Ejercicio 4/src/com/company/Main.java
+++ 04_matriz_3x3/Ejercicio 4/src/com/company/Main.java
@@ -8,24 +8,30 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int filas = 0, columnas = 0;
 
         int[][] matriz = new int[3][3];
 
 
-        for (int i = 0; i < 3; i++) {
-
-
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
                 System.out.print("Escribe un numero: ");
                 int numero = Integer.parseInt(br.readLine());
                 matriz[i][j] = numero;
-                System.out.println(matriz[i][j]);
+
 
             }
         }
+        //visualizar
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.format("%4d", matriz[i][j]);
 
-        System.out.println(" " + matriz[0][0] + " " + matriz[0][1] + " " + matriz[0][2] + "\n" + " " + matriz[1][0] + " " + matriz[1][1] + " " + matriz[1][2] + "\n" + " " + matriz[2][0] + " " + matriz[2][1] + " " + matriz[2][2] + "\n");
+            }
+            System.out.println();
+        }
+
+       /* System.out.println(" " + matriz[0][0] + " " + matriz[0][1] + " " + matriz[0][2] + "\n" + " " + matriz[1][0] + " " + matriz[1][1] + " " + matriz[1][2] + "\n" + " " + matriz[2][0] + " " + matriz[2][1] + " " + matriz[2][2] + "\n");
+        */
 
     }
 }
